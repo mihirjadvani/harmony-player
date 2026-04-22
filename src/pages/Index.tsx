f7f1a8d419cf0423842b37ad07891b1e5a78fd32
mihirjadvani <129718733+mihirjadvani@users.noreply.github.com>
2026-04-22 11:07:25 +0530

    Update Index.tsx

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -37,7 +37,7 @@ const InnerApp = () => {
 
         {/* App Header */}
         <header className="relative px-4 pt-4 pb-2 flex items-center justify-center">
-          <h4 className="text-4xl font-extrabold text-yellow-400 tracking-wide">
+          <h4 className="text-4xl font-Victory Striker Sans text-yellow-400 tracking-wide">
   Harmony Player
 </h4>
         </header>
